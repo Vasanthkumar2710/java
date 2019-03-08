5bc7b7072d2cae2234f8829b53b98217ba8dc324
Vasanthkumar2710 <47375863+Vasanthkumar2710@users.noreply.github.com>
2019-03-08 14:37:12 +0530

    Update VowelorConsont.java

--- VowelorConsont.java
+++ VowelorConsont.java
@@ -19,7 +19,7 @@ class Ideone
 		}
 		else if (ch!='A'||ch!='E'||ch!='I'||ch!='O'||ch!='U'||ch!='a'||ch!='e'||ch!='i'||ch!='o'||ch!='u')
 		{
-			System.out.println("Consonent");
+			System.out.println("Consonant");
 		}
 		else 
 		{
