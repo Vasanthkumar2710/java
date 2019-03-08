e2cd756955537049e65f1a339f6b35469991cc76
Vasanthkumar2710 <47375863+Vasanthkumar2710@users.noreply.github.com>
2019-03-08 15:39:10 +0530

    Create LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		if (b<=a && c<=a)
+		{
+			System.out.println(+a);
+		}
+		else if(b>=a && b>=c)
+		{
+			System.out.println(+b);
+		}
+		else if (c>=a && c>=b)
+		{
+			System.out.println(+c);
+		}
+	}
+}
