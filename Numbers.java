d46f946a7eba1c92b4c9dc1aa36fc5964c865755
Vasanthkumar2710 <47375863+Vasanthkumar2710@users.noreply.github.com>
2019-03-09 15:26:21 +0530

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			int a=0;
+		Scanner s=new Scanner(System.in);
+		int K=s.nextInt();
+	    for (int i=1;i<=K;i++)
+	    {
+	    	System.out.println(+i);
+	   }
+	    int n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		 a=a+i;
+		System.out.println(+a);
+		
+	}
+}
